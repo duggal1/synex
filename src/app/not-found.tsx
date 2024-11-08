76ef30b7954a24f3e843cce182e4ac96250c68cb
harshit duggal <harshitduggal56@gmail.com>
2024-11-08 23:17:14 +0530

    inilized loadbalancer function✅

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,24 +3,24 @@ import Link from "next/link";
 //
 const NotFound = () => {
   return (
-    <main className="relative flex flex-col justify-center items-center h-screen px-4 bg-gradient-to-br from-blue-400 to-purple-500">
-      <div className="flex flex-col justify-center items-center max-w-xl mx-auto text-center">
-        <span className="px-3.5 py-1 mb-5 font-medium text-xl text-white bg-black/20 rounded-md">
+    <main className="relative flex flex-col justify-center items-center bg-gradient-to-br from-blue-400 to-purple-500 px-4 h-screen">
+      <div className="flex flex-col justify-center items-center mx-auto max-w-xl text-center">
+        <span className="bg-black/20 mb-5 px-3.5 py-1 rounded-md font-medium text-white text-xl">
           404
         </span>
-        <h1 className="mt-2 text-6xl lg:text-9xl font-bold text-white md:text-8xl tracking-tight">
+        <h1 className="mt-2 font-bold text-6xl text-white md:text-8xl lg:text-9xl tracking-tight">
           Page Not Found
         </h1>
-        <p className="mt-5 text-base font-medium text-neutral-300 ">
+        <p className="mt-5 font-medium text-base text-neutral-300">
           The page you are looking for does not exist. But don&apos;t worry, you can{" "}
-          <Link href="/dashboard" className="text-white underline hover:to-blue-800 hover:underline">
+          <Link href="/dashboard" className="hover:to-blue-800 text-white underline hover:underline">
             go to the dashboard
           </Link>{" "}
           or{" "}
-          <Link href="/blogs" className="text-white underline ">
+          <Link href="/blogs" className="text-white underline">
             check out our blogs.
           </Link>
-          <span className=" -mt-8 ">
+          <span className="-mt-8">
             ✨
           </span>
           
