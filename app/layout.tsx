997c9cb3f55d468c9ca9bf9489844666d95234b4
harshit duggal <harshitduggal56@gmail.com>
2025-03-13 22:26:55 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { SessionProvider } from "./components/SessionProvider";
-import { generateMetadata } from "./utils";
+import { generateMetadata } from "./utils/metadata";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
